e3cd1d1c88dbf26f1305c3aa9b2793d31a451667
Abhishek Raj <abhishek.raj@intimetec.com>
2023-09-18 23:18:36 +0530

    initial commit

---
+++ src/test/java/com/ss/suraksha/SurakshaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ss.suraksha;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SurakshaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
